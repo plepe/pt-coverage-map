0930d7eed7d0d1e3fed70a4a3fea3c6585705aa5
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-09-15 20:37:06 +0200

    Use basemap.at as background, attribution for coverageLayer

--- src/index.js
+++ src/index.js
@@ -18,21 +18,24 @@ const routeTypes = {
 window.onload = function () {
   var map = L.map('map')
 
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map);
+  L.tileLayer("https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
+      subdomains : ['maps', 'maps1', 'maps2', 'maps3', 'maps4'],
+      attribution: '&copy; <a href="http://basemap.at">Basemap.at</a>'
+  }).addTo(map)
 
   let coverageLayer1 = L.TileLayer.maskCanvas({
     radius: 600,
     useAbsoluteRadius: true,
-    opacity: 0.3
+    opacity: 0.3,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   })
   map.addLayer(coverageLayer1)
 
   let coverageLayer2 = L.TileLayer.maskCanvas({
     radius: 300,
     useAbsoluteRadius: true,
-    opacity: 0.2
+    opacity: 0.2,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   })
   map.addLayer(coverageLayer2)
 
